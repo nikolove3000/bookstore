a57479529913afa5e085971a77c2555424feff9f
Nikolove3000 <mynamelathanh193@gmail.com>
2026-03-30 15:17:19 +0700

    fix: add @PrePersist for createdAt in User entity

--- src/main/java/com/thanh/bookstore/entity/User.java
+++ src/main/java/com/thanh/bookstore/entity/User.java
@@ -123,6 +123,9 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviews;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RefreshToken> refreshTokens;
+
     /**
      * Returns authorities granted to the user.
      *
