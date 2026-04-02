955d3d743137a13720d72e8b9a4af7563a5c9427
Nikolove3000 <mynamelathanh193@gmail.com>
2026-04-02 10:08:35 +0700

    chore: update App routes and global styles

--- bookstore-fe/src/App.jsx
+++ bookstore-fe/src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import LoginPage from './page/LoginPage'
 import HomePage from './page/HomePage'
+import RegisterPage from './page/RegisterPage'
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+          
           <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
